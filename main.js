66daab283f712e08605680ff31ef0478c61a5094
Alexander Miseler <github@miseler.de>
2024-05-15 22:11:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -38,9 +38,9 @@ setInterval(function() {
   }
   else {
     LEDon = !LEDon;
-    NC.ledTop([0,255*LEDon,255*!LEDon]);
-    NC.ledBottom([0,255*!LEDon,255*LEDon]);
-    var backlight = Array(12).fill(0);
+    //NC.ledTop([0,255*LEDon,255*!LEDon]);
+    //NC.ledBottom([0,255*!LEDon,255*LEDon]);
+    var backlight = Array(12).fill(100);
     backlight[0]=0;
     backlight[1]=255*!LEDon;
     backlight[2]=255*LEDon;
